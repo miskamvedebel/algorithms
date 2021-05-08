a9f4e5b8bf9eb353acbeff2abc18788219b2e790
mlebedev <miskamvedebel@gmail.com>
2021-05-08 12:52:49 +0200

    adding dp solution

--- dynamic_programming/matrix_block_sum.py
+++ dynamic_programming/matrix_block_sum.py
@@ -1,3 +1,4 @@
+import random
 '''
 Given a m x n matrix mat and an integer k, return a matrix answer where each answer[i][j] is the sum of all elements mat[r][c] for:
 
@@ -8,6 +9,7 @@ j - k <= c <= j + k, and
 
 def slow_block_sum(mat: list, K: int) -> list:
     '''Receives matrix and returns matrix of the same size with block sums
+    Worst case O(n^4)
     '''
 
     m, n = len(mat), len(mat[0])
@@ -22,7 +24,28 @@ def slow_block_sum(mat: list, K: int) -> list:
                     tab[i][j] += mat[ii][jj]
     return tab
 
+def dp_block_sum(mat: list, K: int) -> list:
+    '''Using dynamic programming
+    '''
+    m, n = len(mat), len(mat[0])
+    tab = [[0] * (n + 1) for _ in range(m + 1)]
+
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            tab[i][j] = mat[i-1][j-1] + tab[i-1][j]  + tab[i][j-1] - tab[i-1][j-1]
+
+    ans = mat.copy()
+
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            r = max(i - K - 1, 0), min(i + K, m)
+            c = max(j - K - 1, 0), min(j + K, n)
+            ans[i-1][j-1] = tab[r[1]][c[1]] + tab[r[0]][c[0]] - tab[r[0]][c[1]] - tab[r[1]][c[0]]
+    
+    return ans
+
 if __name__ == '__main__':
     mat = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     K = 1
     print(slow_block_sum(mat, K))
+    print(dp_block_sum(mat, K))
