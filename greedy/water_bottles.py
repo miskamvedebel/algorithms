fae573ff78ff534eb8527d3e3185332babec4887
maksim.lebedev <maksim.lebedev@tui.com>
2021-05-10 21:39:08 +0200

    solving bottles problem - NOT OPTIMAL

---
+++ greedy/water_bottles.py
@@ -0,0 +1,17 @@
+def water_bottles_queue(numBottles: int, numExchange: int) -> int:
+    
+    bottles = [1] * numBottles
+    empty = 0
+    total = 0
+    while bottles:
+        empty += bottles.pop()
+        total += 1
+        if empty == numExchange:
+            bottles.append(1)
+            empty = 0
+    return total
+
+if __name__ == '__main__':
+
+    print(water_bottles_queue(9, 3)) # should print 13
+    print(water_bottles_queue(9, 3) == 13)
